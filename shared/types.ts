bb88c179b8a7b30d9f4d3d5a9973d6c78659ba37
Yiming Su <yimingsu2007@gmail.com>
2026-02-06 10:00:00 -0800

    define emperor interface

---
+++ shared/types.ts
@@ -0,0 +1,15 @@
+export interface Emperor {
+    name: string;
+    dynasty: string;
+    dynastyIndex: number;
+    reignStartYear: number;
+    reignLengthDays: number;
+    succession: string;
+    fate: string;
+    birthplace: string;
+    religion: string;
+    portrait: string;
+    hint: string;
+    description: string;
+    hide: boolean;
+}
