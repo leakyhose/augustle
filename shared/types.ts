317029cead6bcaf1bd0204f5528b3d453341279d
Yiming Su <yimingsu2007@gmail.com>
2026-02-06 16:45:00 -0800

    add feedback and gamemode types

--- shared/types.ts
+++ shared/types.ts
@@ -1,3 +1,5 @@
+export type GameMode = 'classic' | 'byzantine' | 'all' | 'bust';
+
 export interface Emperor {
     name: string;
     dynasty: string;
@@ -13,3 +15,15 @@ export interface Emperor {
     description: string;
     hide: boolean;
 }
+
+export type Feedback = {
+    name: false | "close-name" | { name: string; portrait: string };
+    dynasty: "earlier" | "later" | { correct: string };
+    reignStart: "earlier" | "later" | "earlier-close" | "later-close" | { correct: number };
+    length: "shorter" | "longer" | "shorter-close" | "longer-close" | { correct: number };
+    succession: "incorrect" | { correct: string };
+    fate: "incorrect" | { correct: string };
+    birthplace: "incorrect" | "close" | { correct: string };
+    religion: "incorrect" | "close" | { correct: string };
+}
+
